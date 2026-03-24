1e8e4823052110c5d2577bb8eb691a5471796e46
Artem Chesanovskyi <artem.chesanovskyi.ki.2022@lpnu.ua>
2026-03-24 20:03:42 +0200

    Add config.js for Vercel deployment and update gitignore

---
+++ js/config.js
@@ -0,0 +1,3 @@
+// config.js - Конфіденційні налаштування (не комітити!)
+// Замініть 'YOUR_GOOGLE_CLIENT_ID' на ваш реальний Google Client ID
+export const googleClientId = 'YOUR_GOOGLE_CLIENT_ID';
